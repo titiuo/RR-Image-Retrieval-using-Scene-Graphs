d5679486be2436f00ef3aeeef02a437692e24b34
Titouan Duhaze <titouanduhaze@w-147-168.wfer.ens-paris-saclay.fr>
2026-01-27 17:22:51 +0100

    main

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+if __name__ == "__main__":
+    #create a txt
+    with open("test.txt", "w") as f:
+        f.write("This is a test file.")
+    print("Test file created.")
+    
